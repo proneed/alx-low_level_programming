e9d189cae084c57e4508a4b86a456304a3632d0d
Nebiyou Yemam <proneed@yahoo.com>
2022-04-08 06:00:22 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/Correct)
+ */
+int main(void)
+
+{
+char i;
+
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);i
+}
+
+putchar('\n');
+
+return (0);
+}
