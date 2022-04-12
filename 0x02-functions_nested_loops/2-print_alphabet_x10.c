ae95b52551e267fb57045cc9802ba05e1b264d50
Nebiyou Yemam <proneed@yahoo.com>
2022-04-12 03:54:29 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,8 +7,9 @@
 
 void print_alphabet_x10(void)
 {
-int c = 0;
-char alphabet;
+int c;
+c = 0;
+int alphabet;
 while (c < 10)
 {
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
@@ -16,7 +17,7 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 _putchar(alphabet);
 }
 c++;
-}
 _putchar('\n');
+}
 }
 
