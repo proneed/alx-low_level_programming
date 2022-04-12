d3da1b1207731484feefc7c1b6b93a511be47e5f
Nebiyou Yemam <proneed@yahoo.com>
2022-04-12 07:05:49 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -prints add two numbers
+ *@i: print int a
+ *@k: print int b
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
