29545c95454f5c238a69c3e9497f8dda65820119
Nebiyou Yemam <proneed@yahoo.com>
2022-04-12 04:01:57 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_10x - prints 10 times the alphabet
+ * print_alphabet_x10 - print alphabet
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
