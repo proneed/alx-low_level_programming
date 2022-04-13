cf0473849136716fd43401ea736db5748e14065c
Nebiyou Yemam <proneed@yahoo.com>
2022-04-13 15:54:19 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
-#include <stdlib.h>
-#include <time.h>
+/*#include <stdlib.h>*/
+/*#include <time.h>*/
 #include <stdio.h>
 
 /**
@@ -11,8 +11,8 @@
 int main(void)
 {
 int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+/*srand(time(0));*/
+/*n = rand() - RAND_MAX / 2;*/
 if (n == 0)
 {
 printf("%i is zero\n", n);
