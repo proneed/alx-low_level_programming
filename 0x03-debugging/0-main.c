29c0ce2ba5521009f2cc56d71e58a24fda6c8208
Nebiyou Yemam <proneed@yahoo.com>
2022-04-13 13:29:33 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
  * main - tests function prints if integer positive or negative
- *
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-	return (0);
+return (0);
 }
