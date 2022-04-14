2e4bdf503868573ec4229d33309dcd93b4b72afa
Nebiyou Yemam <proneed@yahoo.com>
2022-04-14 11:41:47 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,14 +7,14 @@
 
 int _isdigit(int c);
 {
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
 
-	else
-	{
-		return (0);
-	}
+else
+{
+return (0);
+}
 
 }
