3a8e7705723cfecf1828088f8ef2d7986af03eac
Nebiyou Yemam <proneed@yahoo.com>
2022-04-14 11:28:02 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,10 @@
 
 /**
  * _isupper - checks for uppercase character
- * @letter: the character to be tracked
+ * @c: the character to be tracked
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int letter)
+int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
 {
