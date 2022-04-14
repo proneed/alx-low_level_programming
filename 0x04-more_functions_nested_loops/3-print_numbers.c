26fb588f10bdaf7de863b997ed38c284e72275a3
Nebiyou Yemam <proneed@yahoo.com>
2022-04-14 12:22:01 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,12 @@
 
 void print_numbers(void)
 {
-	int i;
-	for (i = 48; i <= 57; i++)
-	{
-		_putchar(i);
-	}
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+}
 
-	_putchar('\n');
+_putchar('\n');
+
 }
