337a4bffde63e7e596d92c9ff8f2c0aec273cb55
Nebiyou Yemam <proneed@yahoo.com>
2022-04-14 06:46:11 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - checks if the letter is upper or not
+ *
+ * Return: Always 0
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
